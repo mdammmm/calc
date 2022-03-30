2954637360be3072a5fc6df3dec11314ad8b95f7
Admin <admin@Mac.local>
2022-03-30 09:53:15 +0300

    First message

---
+++ calc(Romanova)/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  calc(Romanova)
+//
+//  Created by Admin on 30.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var buttonSlash: UIButton!
+    @IBOutlet weak var buttonAC: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        buttonAC.layer.cornerRadius = 36
+        
+    }
+
+
+}
+
